a78cca64c31e905dedd49b41479dca7475c401e3
Vinícius Yoda <vyoda4604@gmail.com>
2023-02-11 13:44:02 -0300

    mostraItem

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ let listaDeItens = []
 
 const form = document.getElementById('form-itens')
 const itensInput = document.getElementById('receber-item')
+const ulItens = document.getElementById('lista-de-itens')
+const ulItensCOmprados = document.getElementById('itens-comprados')
 
 form.addEventListener("submit", function(evento) {
    evento.preventDefault()
    salvarItem()
+   mostrarItem()
+   itensInput.focus()
 })
 
 function salvarItem() {
@@ -16,9 +20,54 @@ function salvarItem() {
       alert("Item já existe")
    } else {
       listaDeItens.push({
-         valor: comprasItem
+         valor: comprasItem,
+         checar: false
       })
    }
-
    
+   itensInput.value = ''
+   
+}
+
+function mostrarItem() {
+   ulItens.innerHTML = ''
+   ulItensCOmprados.innerHTML = ''
+   listaDeItens.forEach((elemento, index) => {
+      if(elemento.checar) {
+         ulItensCOmprados.innerHTML = `
+            <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+            <div>
+               <input type="checkbox" checked class="is-clickable" />  
+               <span class="itens-comprados is-size-5">${elemento.valor}</span>
+            </div>
+            <div>
+               <i class="fa-solid fa-trash is-clickable deletar"></i>
+            </div>
+      </li>
+         `
+      } else {
+         ulItens.innerHTML += `
+         <li class="item-compra is-flex is-justify-content-space-between" data-value=${index}">
+            <div>
+               <input type="checkbox" class"is-clickable" />
+               <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+            </div>
+            <div>
+               <i class="fa-solid fa-trash is-clickable deletar"></i>
+            </div>
+         </li>
+      `
+      }
+      
+   })
+
+   const inputsCheck = document.querySelectorAll('input[type="checkbox"]')
+
+   inputsCheck.forEach(i => {
+      i.addEventListener('click', (evento) => {
+         const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+         listaDeItens[valorDoElemento].checar = evento.target.checked
+         mostrarItem()
+      })
+   })
 }
